bab410c7496f6d5d552182ff9899c61ef5eb9920
RR <2683524740@qq.com>
2024-08-17 18:42:45 +0800

    first commit

---
+++ Test/test.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <stdio.h>
+#include "max.h"
+using namespace std;
+
+int main(){
+    int a = 1, b = 2;
+    cout << "The maximum value of a & b is: " << max(a, b) << endl;
+    cout << "hello cpp" << endl;
+    printf("hello cpp_1");
+    return 0;
+}
